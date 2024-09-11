5a756d8844384e94f1b753fb82e3892969b7395c
Benjamin Maurer <benjamin.maurer@radancy.com>
2024-09-11 13:31:28 +0200

    Update plugins, refactor build.

--- project/PublishSettings.scala
+++ project/PublishSettings.scala
@@ -1,43 +1,44 @@
-import sbt._
-import sbt.Keys._
-import bintray.{BintrayKeys, BintrayPlugin}
+// import sbt._
+// import sbt.Keys._
+// import bintray.{BintrayKeys, BintrayPlugin}
 
-object PublishSettings extends AutoPlugin with BintrayKeys {
-  override def trigger = allRequirements
-  override def requires = plugins.JvmPlugin && sbtrelease.ReleasePlugin && BintrayPlugin && ProjectSettings
+object PublishSettings {}
+//  extends AutoPlugin with BintrayKeys {
+//   override def trigger = allRequirements
+//   override def requires = plugins.JvmPlugin && sbtrelease.ReleasePlugin && BintrayPlugin
 
- override def projectSettings = Seq(
-    publishMavenStyle := true,
-    pomExtraSetting("akka-persistence-inmemory"),
-    homepageSetting("akka-persistence-inmemory"),
-    bintrayPackageLabelsSettings("inmemory"),
-    bintrayPackageAttributesSettings("akka-persistence-inmemory")
- )
-  
-def pomExtraSetting(name: String) = pomExtra := 
-    <scm>
-        <url>https://github.com/dnvriend/${name}</url>
-        <connection>scm:git@github.com:dnvriend/${name}.git</connection>
-        </scm>
-        <developers>
-        <developer>
-            <id>dnvriend</id>
-            <name>Dennis Vriend</name>
-            <url>https://github.com/dnvriend</url>
-        </developer>
-        </developers>
+//  override def projectSettings = Seq(
+//     publishMavenStyle := true,
+//     pomExtraSetting("akka-persistence-inmemory"),
+//     homepageSetting("akka-persistence-inmemory"),
+//     bintrayPackageLabelsSettings("inmemory"),
+//     bintrayPackageAttributesSettings("akka-persistence-inmemory")
+//  )
 
-    def homepageSetting(name: String) = 
-      homepage := Some(url(s"https://github.com/dnvriend/$name"))
+// def pomExtraSetting(name: String) = pomExtra :=
+//     <scm>
+//         <url>https://github.com/dnvriend/${name}</url>
+//         <connection>scm:git@github.com:dnvriend/${name}.git</connection>
+//         </scm>
+//         <developers>
+//         <developer>
+//             <id>dnvriend</id>
+//             <name>Dennis Vriend</name>
+//             <url>https://github.com/dnvriend</url>
+//         </developer>
+//         </developers>
 
-    def bintrayPackageLabelsSettings(labels: String*) = 
-	  bintrayPackageLabels := Seq("akka", "persistence") ++ labels
+//     def homepageSetting(name: String) =
+//       homepage := Some(url(s"https://github.com/dnvriend/$name"))
 
-    def bintrayPackageAttributesSettings(name: String) = {
-      bintrayPackageAttributes ~= (_ ++ Map(
-          "website_url" -> Seq(bintry.Attr.String(s"https://github.com/dnvriend/$name")),
-          "github_repo" -> Seq(bintry.Attr.String(s"https://github.com/dnvriend/$name.git")),
-          "issue_tracker_url" -> Seq(bintry.Attr.String(s"https://github.com/dnvriend/$name.git/issues/"))
-        ))
-    }
-}
+//     def bintrayPackageLabelsSettings(labels: String*) =
+// 	  bintrayPackageLabels := Seq("akka", "persistence") ++ labels
+
+//     def bintrayPackageAttributesSettings(name: String) = {
+//       bintrayPackageAttributes ~= (_ ++ Map(
+//           "website_url" -> Seq(bintry.Attr.String(s"https://github.com/dnvriend/$name")),
+//           "github_repo" -> Seq(bintry.Attr.String(s"https://github.com/dnvriend/$name.git")),
+//           "issue_tracker_url" -> Seq(bintry.Attr.String(s"https://github.com/dnvriend/$name.git/issues/"))
+//         ))
+//     }
+// }
